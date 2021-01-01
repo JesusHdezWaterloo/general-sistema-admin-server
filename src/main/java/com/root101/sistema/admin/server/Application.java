896bda235dff79cda238e694ee349405bb2ec743
Unknown <unknown@example.com>
2021-01-01 17:20:24 -0500

    Licence a App

--- src/main/java/com/root101/sistema/admin/server/Application.java
+++ src/main/java/com/root101/sistema/admin/server/Application.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.root101.sistema.admin.server;
 
 import com.root101.module.gestion.contabilidad.repo.utils.ResourcesContabilidad;
@@ -21,6 +37,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ *
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = {
     A_ModuleGestionContabilidadEmpresarial.BASE_PACKAGE,
@@ -28,8 +49,7 @@ import org.springframework.web.bind.annotation.*;
     A_ModuleAdminKanban.BASE_PACKAGE,
     A_ModuleOAuth2.BASE_PACKAGE,
     A_ModuleAdminSeguridad.BASE_PACKAGE,
-    A_ModuleUtilLicence.BASE_PACKAGE,
-})
+    A_ModuleUtilLicence.BASE_PACKAGE,})
 @RestController
 @RequestMapping(value = "/admin")
 public class Application extends SpringBootServletInitializer {
